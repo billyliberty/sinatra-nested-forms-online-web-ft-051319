ba891e10f0f2f374f906bf99e8f1d3458abd6b31
Joshua Crum <joshua.crum@gmail.com>
2019-07-01 16:03:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
     get '/' do
-      erb :'pirates/new'
+      erb :root
     end
 
     get '/new' do
-      erb :root
+      erb :'pirates/new'
     end
 
     post '/pirates' do
